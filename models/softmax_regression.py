6b7b44651da68a6c3944f21349741f8000e53c2c
Willam Tran <quangminhtran2006@gmail.com>
2026-04-19 12:59:16 +0700

    Fix overflown with logsumexp()

--- models/softmax_regression.py
+++ models/softmax_regression.py
@@ -10,44 +10,47 @@ class SoftmaxRegression:
         self.losses = []
         self.w = None
 
-    def loss_fn(self, y: np.ndarray, y_hat: np.ndarray) -> float:
-        return -(y * np.log(y_hat + 1e-15)).sum(axis=1).mean()
+    def loss_fn(self, y: np.ndarray, log_y_hat: np.ndarray) -> float:
+        return -(y * log_y_hat).sum(axis=1).mean()
 
     def fit(self, X: np.ndarray, y: np.ndarray):
         N, d = X.shape
         C = y.shape[1]
         self.w = np.zeros((d, C), dtype=np.float64)
-        e = 0
+        
         with tqdm(range(self.epoch)) as pbar:
             for _ in pbar:
-                y_hat = self.predict_proba(X)
+                log_y_hat = self.predict_log_proba(X)
+                y_hat = np.exp(log_y_hat)
                 delta_y = (y_hat - y)
                 
                 gradient = X.T @ delta_y / N
                 self.w -= gradient * self.lr
 
-                l = self.loss_fn(y, y_hat)
+                l = self.loss_fn(y, log_y_hat)
                 self.losses.append(l)
 
                 pbar.set_postfix({
                     "loss": l
                 })
 
-                e += 1
-
         return self.losses
 
-    def softmax(self, X: np.ndarray):
-        X = X - np.max(X, axis=-1, keepdims=True)
-        exp = np.exp(X)
-        return exp / exp.sum(axis=-1, keepdims=True)
+    def logsumexp(self, X: np.ndarray):
+        a = X.max(-1)
+        return a + np.log(np.exp(X - a[:, None]).sum(-1))
+    
+    def log_softmax(self, X: np.ndarray):
+        return X - self.logsumexp(X)[:, None]
+
+    def predict_log_proba(self, X: np.ndarray):
+        return self.log_softmax(X @ self.w)
 
     def predict_proba(self, X: np.ndarray):
-        y_hat = X @ self.w
-        return self.softmax(y_hat)
+        return np.exp(self.predict_log_proba(X))
     
     def predict(self, X):
-        return np.argmax(self.predict_proba(X), axis=1)
+        return np.argmax(self.predict_log_proba(X), axis=1)
     
     def evaluate(self, y, y_hat) -> dict:
         y_true = np.argmax(y, axis=1)
